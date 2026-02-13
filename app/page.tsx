2618893827b881004dee94da6f06b1af8f12829a
Mike Quisido <mike082112@gmail.com>
2026-02-14 07:43:11 +0800

    Adjust logo spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-start justify-start pt-15 pl-15">
+    <div className="flex min-h-screen items-start justify-start pt-5 pl-5">
       <div className="flex items-center gap-2 pl-[5px] pt-[5px]">
         <Image
           src="/kmcq-cloud-company-white-logo.png"
@@ -12,7 +12,7 @@ export default function Home() {
           className="w-[100px] sm:w-[120px] md:w-[157px]"
           style={{ height: 'auto' }}
         />
-        <span className="font-ubuntu text-white text-[24px] sm:text-[30px] md:text-[40px] font-bold tracking-wide translate-y-[3px]">
+        <span className="font-ubuntu text-white text-[24px] sm:text-[30px] md:text-[40px] font-bold tracking-wide translate-x-[10px] translate-y-[35px]">
           KMCQ COMPANY
         </span>
       </div>
