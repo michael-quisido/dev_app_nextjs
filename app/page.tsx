e13a0f0f73eb8c108b55b5975fb2c425ee15590a
Mike Quisido <mike082112@gmail.com>
2026-02-14 07:24:42 +0800

    Make homepage mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-start pt-15 pl-15">
-      <div className="flex items-center gap-3 pl-[7px] pt-[7px]">
+      <div className="flex items-center gap-2 pl-[7px] pt-[7px]">
         <Image
           src="/kmcq-cloud-company-white-logo.png"
           alt="KMCQ Company Logo"
           width={157}
           height={0}
+          className="w-[100px] sm:w-[120px] md:w-[157px]"
           style={{ height: 'auto' }}
         />
-        <span className="font-ubuntu text-white text-[40px] font-bold tracking-wide translate-y-[3px]">
+        <span className="font-ubuntu text-white text-[24px] sm:text-[30px] md:text-[40px] font-bold tracking-wide translate-y-[3px]">
           KMCQ COMPANY
         </span>
       </div>
