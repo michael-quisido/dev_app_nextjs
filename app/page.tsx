0af4cfd7d25f5792d8458194539c23cb0d3879ec
Mike Quisido <mike082112@gmail.com>
2026-02-14 07:17:13 +0800

    Adjust logo spacing, make text bold and lower 3px

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-start pt-15 pl-15">
-      <div className="flex items-center gap-3 pl-[15px] pt-[15px]">
+      <div className="flex items-center gap-3 pl-[7px] pt-[7px]">
         <Image
           src="/kmcq-cloud-company-white-logo.png"
           alt="KMCQ Company Logo"
@@ -11,7 +11,7 @@ export default function Home() {
           height={0}
           style={{ height: 'auto' }}
         />
-        <span className="font-ubuntu text-white text-[40px] font-normal tracking-wide">
+        <span className="font-ubuntu text-white text-[40px] font-bold tracking-wide translate-y-[3px]">
           KMCQ COMPANY
         </span>
       </div>
