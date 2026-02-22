e7ea4e5a7d1d107429a55c9de011272cc3ee7e11
Mike Quisido <mike082112@gmail.com>
2026-02-22 18:17:18 +0800

    Fix TypeScript error for react-icons

---
+++ types/react-icons.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-icons/fa' {
+  import { IconType } from 'react-icons';
+  export const FaLinkedin: IconType;
+  export const FaFacebook: IconType;
+  export const FaInstagram: IconType;
+  export const FaYoutube: IconType;
+  export const FaSearch: IconType;
+  export const FaBars: IconType;
+  export const FaTimes: IconType;
+}
